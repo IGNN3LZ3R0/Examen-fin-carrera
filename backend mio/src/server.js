7a2f69bc90809a4245bbc3adf0dc9d115be683e9
IGNN3LZ3R0 <ignnel01end@gmail.com>
2025-02-22 16:48:55 -0500

    configuracion de database, server y el index

---
+++ backend mio/src/server.js
@@ -0,0 +1,27 @@
+// Requerir los módulos
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors';
+import routerusuario from "./routers/usuarioroutes.js"
+
+// Inicializaciones
+const app = express()
+dotenv.config()
+
+// Configuraciones 
+app.set('port',process.env.port || 3000)
+app.use(cors())
+
+// Middlewares 
+app.use(express.json())
+
+// Rutas 
+app.get('/',(req,res)=>{
+    res.send("Server on")
+})
+
+app.use("/caso1", routerusuario)
+
+
+// Exportar la instancia de express por medio de app
+export default  app
