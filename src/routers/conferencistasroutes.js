61c78bf8ae8494ebb26a4884780b9f8bb6bbc166
IGNN3LZ3R0 <ignnel01end@gmail.com>
2025-03-05 10:10:41 -0500

    correccion conferencistas rutas

--- src/routers/conferencistasroutes.js
+++ src/routers/conferencistasroutes.js
@@ -14,9 +14,9 @@ router.get('/matriculas/bienvenido', verificarJWT, (req, res) => {
     res.json({ msg: `Bienvenido - ${req.user.nombre}` });
 });
 // Rutas CRUD para estudiantes
-router.post('/estudiantes/crear', verificarJWT, CrearConferencista); // Crear un nuevo conferencista
-router.get('/estudiantes/ver', verificarJWT, VerConferencistas); // Obtener todos los conferencistas
-router.get('/estudiantes/ver/:id', verificarJWT, DetalleConferencista); // Obtener detalle de un confencistas
-router.put('/estudiantes/actualizar/:id', verificarJWT, ActualizarConferencista,); // Actualizar información de un conferencista
-router.delete('/estudiantes/eliminar/:id', verificarJWT, EliminarConferencista,); // Eliminar un conferencista
+router.post('/conferencista/crear', verificarJWT, CrearConferencista); // Crear un nuevo conferencista
+router.get('/conferencista/ver', verificarJWT, VerConferencistas); // Obtener todos los conferencistas
+router.get('/conferencista/ver/:id', verificarJWT, DetalleConferencista); // Obtener detalle de un confencistas
+router.put('/conferencista/actualizar/:id', verificarJWT, ActualizarConferencista,); // Actualizar información de un conferencista
+router.delete('/conferencista/eliminar/:id', verificarJWT, EliminarConferencista,); // Eliminar un conferencista
 export default router;
