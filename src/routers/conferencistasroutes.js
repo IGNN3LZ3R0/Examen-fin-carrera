8b28102b6f45130e67ecf4f48ff5444f169378c3
IGNN3LZ3R0 <ignnel01end@gmail.com>
2025-03-05 10:12:47 -0500

    correccion conferencistas rutas 2.0

--- src/routers/conferencistasroutes.js
+++ src/routers/conferencistasroutes.js
@@ -5,18 +5,17 @@ import {
     ActualizarConferencista,
     EliminarConferencista,
     DetalleConferencista,
-}from '../controllers/conferencistascontroller.js'; // Asegúrate de que el nombre del archivo sea correcto
+} from '../controllers/conferencistascontroller.js'; // Asegúrate de que el nombre del archivo sea correcto
 import verificarJWT from '../middlewares/verificarJWT.js';
 const router = express.Router();
-// Ruta para la bienvenida
 // Ruta de bienvenida
 router.get('/matriculas/bienvenido', verificarJWT, (req, res) => {
     res.json({ msg: `Bienvenido - ${req.user.nombre}` });
 });
-// Rutas CRUD para estudiantes
+// Rutas CRUD para conferencistas
 router.post('/conferencista/crear', verificarJWT, CrearConferencista); // Crear un nuevo conferencista
 router.get('/conferencista/ver', verificarJWT, VerConferencistas); // Obtener todos los conferencistas
-router.get('/conferencista/ver/:id', verificarJWT, DetalleConferencista); // Obtener detalle de un confencistas
-router.put('/conferencista/actualizar/:id', verificarJWT, ActualizarConferencista,); // Actualizar información de un conferencista
-router.delete('/conferencista/eliminar/:id', verificarJWT, EliminarConferencista,); // Eliminar un conferencista
+router.get('/conferencista/ver/:id', verificarJWT, DetalleConferencista); // Obtener detalle de un conferencista
+router.put('/conferencista/actualizar/:id', verificarJWT, ActualizarConferencista); // Actualizar información de un conferencista
+router.delete('/conferencista/eliminar/:id', verificarJWT, EliminarConferencista); // Eliminar un conferencista
 export default router;
